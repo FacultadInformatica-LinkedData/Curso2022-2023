60d0f139feb97261d6909c1228b2d8dadfdb1956
Fernando Sánchez <f.sanchez-moralesr@alumnos.upm.es>
2022-10-12 17:30:30 +0200

    app implementation v0

--- HandsOn/Group27/app/model/graph_loader.py
+++ HandsOn/Group27/app/model/graph_loader.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2022 FSM Solutions.
-from rdflib import Graph, Namespace
+from rdflib import Graph, Namespace, RDFS
+import pathlib
 
 graph = Graph()
-graph.parse("./HandsOn/Group27/rdf/consumo-energia-edificios-updated.ttl", format="ttl")
+graph.parse(pathlib.Path(__file__).parents[2].resolve().as_posix() + "/rdf/consumo-energia-edificios-updated.ttl", format="ttl")
 
 # Used namespaces
 SSN = Namespace("http://www.w3.org/2005/Incubator/ssn/ssnx/ssn#")
@@ -12,11 +13,3 @@ EC = Namespace("http://www.semanticweb_g27.org/ontology/EnergyCons#")
 OWL = Namespace("http://www.w3.org/2002/07/owl#")
 XSD = Namespace("http://www.w3.org/2001/XMLSchema#")
 SCHEMA = Namespace("http://schema.org/")
-
-graph.namespace_manager.bind('ssn', SSN, override=False)
-graph.namespace_manager.bind('time', TIME, override=False)
-graph.namespace_manager.bind('wd', WD, override=False)
-graph.namespace_manager.bind('schema', SCHEMA, override=False)
-graph.namespace_manager.bind('ec', EC, override=False)
-
-# graph loaded.
